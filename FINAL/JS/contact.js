4fbb1268571428adac9cc0b1702edbc5e13a0ee9
YukkiRDZ <thiiagorodriguez.sk8@gmail.com>
2024-08-02 22:53:08 -0300

    final del final

--- FINAL/JS/contact.js
+++ FINAL/JS/contact.js
@@ -6,31 +6,32 @@ let correo = document.getElementById('correo');
 let telefono = document.getElementById('telefono');
 let box = document.getElementById('box');
 let btnEnviar = document.getElementById('enviar');
-let form = document.getElementById('form');
 let informacion = [];
 
-btnEnviar.addEventListener('click', (e) => {
+btnEnviar.addEventListener('submit', (e) => {
     e.preventDefault(); //previene la accion del form de actualizar la pagina
     //validacion de inputs antes de descargar el txt
     if (nombre.value != '' && apellido.value != '' && correo.value != '' && telefono.value != '' && box.value != '') {
         informacion[0] = 'Nombre:' + nombre.value;
-        informacion[1] = 'Apellido:' + apellido.value;
-        informacion[2] = 'Correo:' + correo.value;
-        informacion[3] = 'Telefono:' + telefono.value;
-        informacion[4] = 'Comentario:' + box.value;
-
+        informacion[1] = ' Apellido:' + apellido.value;
+        informacion[2] = ' Correo:' + correo.value;
+        informacion[3] = ' Telefono:' + telefono.value;
+        informacion[4] = ' Comentario:' + box.value;
         let blob = new Blob([informacion], { type: 'text/plain;charset=utf-8' }); //navegador 
-
-        //libreria FileSaver.js
-        saveAs(blob, 'contact.txt'); //recibe el blob y lo guarda en el txt
-        //vaciar inputs luego de apretar el boton
-        nombre.value = '';
-        apellido.value = '';
-        correo.value = '';
-        telefono.value = '';
-        box.value = '';
+        if ((telefono.value).length >= 10) {
+            //libreria FileSaver.js
+            saveAs(blob, 'contact.txt'); //recibe el blob y lo guarda en el txt
+            // vaciar inputs luego de apretar el boton
+            nombre.value = '';
+            apellido.value = '';
+            correo.value = '';
+            telefono.value = '';
+            box.value = '';
+        } else {
+            alert('Recuerde que su numero de telefono debe tener min 10 digitos.');
+        }
     } else {
-        alert('Por favor, debe completar todos los campos antes de enviar');
+        alert('Por favor, debe completar todos los campos antes de enviar.')
     }
 });
 
